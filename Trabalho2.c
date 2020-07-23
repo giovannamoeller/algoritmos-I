d27349116d9084cf39a046665928b1fdade2e2dd
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-23 15:35:27 -0300

    Fazendo funções de troca linha/coluna

--- Trabalho2.c
+++ Trabalho2.c
@@ -22,6 +22,7 @@ int pegaColuna() {
 }
 
 void lerMatriz (int matriz[i][j], int linha, int coluna) {
+    printf("\nDigite os valores da matriz: \n");
     int a, b;
     for (a = 0; a < linha; a++) {
         for(b = 0; b < coluna; b++) {
@@ -39,13 +40,53 @@ void mostrarMatriz (int matriz[i][j], int linha, int coluna) {
     }
 }
 
+void trocarElementosLinha (int matriz[i][j], int linha, int coluna) {
+    int x, y, aux;
+    printf("Digite a linha X: ");
+    scanf("%d", &x);
+    printf("Digite a linha Y: ");
+    scanf("%d", &y);
+    for(int a = 0; a < linha; a++) {
+        for(int b = 0; b < coluna; b++) {
+            if(x == a) {
+                aux = matriz[a][b];
+                matriz[a][b] = matriz[y][b];
+                matriz[y][b] = aux;
+            }
+        }
+        printf("\n");
+    }
+}
+
+void trocarElementosColuna (int matriz[i][j], int linha, int coluna) {
+    int x, y, aux;
+    printf("Digite a coluna X: ");
+    scanf("%d", &x);
+    printf("Digite a coluna Y: ");
+    scanf("%d", &y);
+    for(int a = 0; a < linha; a++) {
+        for(int b = 0; b < coluna; b++) {
+            if(x == b) {
+                aux = matriz[a][b];
+                matriz[a][b] = matriz[a][y];
+                matriz[a][y] = aux;
+            }
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     int linha, coluna;
+    int matriz[i][j];
     printf("Ler Matriz\n\n");
     linha = pegaLinha();
     coluna = pegaColuna();
-    int matriz[i][j];
     lerMatriz(matriz, linha, coluna);
-    printf("Mostrar Matriz\n\n");
+    printf("\nMostrar Matriz\n\n");
+    printf("\nTrocar Elementos da Linha X pela Linha Y\n");
+    trocarElementosLinha(matriz, linha, coluna);
+    printf("\nTrocar Elementos da Coluna X pela Coluna Y\n");
+    trocarElementosColuna(matriz, linha, coluna);
     mostrarMatriz(matriz, linha, coluna);
 }
