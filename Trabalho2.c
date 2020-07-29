ba3f210a3213953265ca64ee38d7060cc2f81088
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-29 10:24:01 -0300

    Finalizando Trabalho 2

--- Trabalho2.c
+++ Trabalho2.c
@@ -1,56 +1,89 @@
 // Aluna: Giovanna Gimenes Moeller
-// Curso: Sistemas de InformaÃ§Ã£o
+// Curso: Sistemas de Informação
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <windows.h>
 #define i 20
 #define j 20
+#define max 100
+#define esc 27
+#define baixo 80
+#define cima 72
+#define enter 13
+#define f1 59
+
+int a;
+short posX = 5, posY = 3;
+
+void moveXY(int x, int y, char t[10])
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD position = {x,y};
+	SetConsoleCursorPosition(hStdout, position);
+	printf("%s", t);	
+}
+
+void ajuda() {
+	system("cls");
+	int tecla;
+	moveXY(10, 2, "AJUDA");
+	moveXY(2, 5, "Utilize as setas (para cima e para baixo) do teclado para navegar entre o menu");
+	moveXY(2, 9, "Utilize as funções desejadas, mas sempre lembrando de primeiro escrever a matriz para não causar problemas");
+	moveXY(15, 17, "Pressione qualquer tecla para voltar ao menu");
+	tecla = getch();
+}
 
 int pegaLinha() {
     int linha;
-    printf("Digite o tamanho total de linhas: ");
+    moveXY(10, 5, "Digite o tamanho total de linhas da matriz: ");
     scanf("%d", &linha);
     return linha;
 }
 
 int pegaColuna() {
     int coluna;
-    printf("Digite o tamanho total de colunas: ");
+    moveXY(10, 10, "Digite o tamanho total de colunas da matriz: ");
     scanf("%d", &coluna);
     return coluna;
 }
 
 void lerMatriz (int matriz[i][j], int linha, int coluna) {
-    int linha, coluna;
-    linha = pegaLinha();
-    coluna = pegaColuna();
-    printf("\nDigite os valores da matriz: \n");
+   	moveXY(20, 5, "Digite os valores da matriz:\n\n");
     int a, b;
     for (a = 0; a < linha; a++) {
         for(b = 0; b < coluna; b++) {
+        	printf ("\n\tElemento[%d][%d] = ", a, b);
             scanf("%d", &matriz[a][b]);
+            printf("\n");
         }
     }
 }
 
 void mostrarMatriz (int matriz[i][j], int linha, int coluna) {
-    for(int a = 0; a < linha; a++) {
-        for(int b = 0; b < coluna; b++) {
-            printf("%d ", matriz[a][b]);
+	int a, b, c;
+	printf("\n\n\n\t\t");
+    for(a = 0; a < linha; a++) {
+        for(b = 0; b < coluna; b++) {
+            printf("%d\t", matriz[a][b]);
         }
-        printf("\n");
+        printf("\n\n\t\t");
     }
+	moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
 }
 
 void trocarElementosLinha (int matriz[i][j], int linha, int coluna) {
-    int x, y, aux;
-    printf("Digite a linha X: ");
+    int x, y, aux, a, b, c;
+    printf("\n\n\tDigite a linha X: ");
     scanf("%d", &x);
-    printf("Digite a linha Y: ");
+    printf("\n\n\tDigite a linha Y: ");
     scanf("%d", &y);
-    for(int a = 0; a < linha; a++) {
-        for(int b = 0; b < coluna; b++) {
+    for(a = 0; a < linha; a++) {
+        for(b = 0; b < coluna; b++) {
             if(x == a) {
                 aux = matriz[a][b];
                 matriz[a][b] = matriz[y][b];
@@ -58,16 +91,18 @@ void trocarElementosLinha (int matriz[i][j], int linha, int coluna) {
             }
         }
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
 }
 
 void trocarElementosColuna (int matriz[i][j], int linha, int coluna) {
-    int x, y, aux;
-    printf("Digite a coluna X: ");
+    int x, y, aux, a, b, c;
+    printf("\n\n\tDigite a coluna X: ");
     scanf("%d", &x);
-    printf("Digite a coluna Y: ");
+    printf("\n\n\tDigite a coluna Y: ");
     scanf("%d", &y);
-    for(int a = 0; a < linha; a++) {
-        for(int b = 0; b < coluna; b++) {
+    for(a = 0; a < linha; a++) {
+        for(b = 0; b < coluna; b++) {
             if(x == b) {
                 aux = matriz[a][b];
                 matriz[a][b] = matriz[a][y];
@@ -75,12 +110,14 @@ void trocarElementosColuna (int matriz[i][j], int linha, int coluna) {
             }
         }
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
 }
 
 void trocarDiagonal(int matriz[i][j], int linha, int coluna) {
-    int a, b, aux, c = coluna - 1;
+    int a, b, aux, c = coluna - 1, t;
     if(linha != coluna) {
-        printf("A troca de diagonal principal pela secundÃ¡ria sÃ³ pode ser feita em matriz de ordem igual. Volte ao menu e reescreve a matriz");
+        printf("\n\n\tA troca de diagonal principal pela secundária só pode ser feita em matriz de ordem igual. Volte ao menu e reescreve a matriz");
     } else {
         for(a = 0; a < linha; a++) {
             for(b = 0; b < coluna; b++) {
@@ -92,13 +129,16 @@ void trocarDiagonal(int matriz[i][j], int linha, int coluna) {
                 }
             }
         }
+        mostrarMatriz(matriz, linha, coluna);
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	t = getch();
 }
 
 void matrizSimetrica(int matriz[i][j], int linha, int coluna) {
-    int a, b, cont = 0;
+    int a, b, cont = 0, c;
     if(linha != coluna) {
-        printf("A matriz simÃ©trica sÃ³ pode ser calculada com matriz quadrada. Volte ao menu e digite uma outra matriz.");
+        printf("\n\n\tA matriz simétrica só pode ser calculada com matriz quadrada. Volte ao menu e digite uma outra matriz.");
     }
     else {
         for(a = 0; a < linha; a++) {
@@ -109,22 +149,26 @@ void matrizSimetrica(int matriz[i][j], int linha, int coluna) {
             }
         }
         if(cont != 0) {
-            printf("A matriz nÃ£o Ã© simÃ©trica\n\n");
-        }
+            printf("\n\n\tA matriz não é simétrica\n\n");
+        } else {
+        	printf("\n\n\tA matriz é simétrica\n\n");	
+		}
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
 }
 
 void matrizQuadradoMagico(int matriz[i][j], int linha, int coluna) {
-    int a, b = 0, soma1 = 0, soma2 = 0, cont = 0, c = coluna - 1, soma3 = 0, soma4 = 0;
+    int a, b = 0, soma1 = 0, soma2 = 0, cont = 0, c = coluna - 1, soma3 = 0, soma4 = 0, t;
     if(linha != coluna) {
-        printf("O quadrado mÃ¡gico sÃ³ pode ser calculado com matriz quadrada. Volte ao menu e digite uma outra matriz.");
+        printf("\n\n\tO quadrado mágico só pode ser calculado com matriz quadrada. Volte ao menu e digite uma outra matriz.");
     } else {
         for(a = 0; a < linha; a++) {
             soma1 += matriz[a][b];
             for(b = 0; b < coluna; b++) {
                 if(a == b) { 
                     soma3 += matriz[a][b]; // soma diagonal principal
-                    soma4 += matriz[a][c]; // soma diagonal secundÃ¡ria
+                    soma4 += matriz[a][c]; // soma diagonal secundária
                     c--;
                 }
                 soma2 += matriz[a][b]; // soma de linhas
@@ -143,56 +187,218 @@ void matrizQuadradoMagico(int matriz[i][j], int linha, int coluna) {
         }
         if(soma1 != soma2 && soma3 != soma4) cont++;
         if(cont != 0) {
-            printf("A matriz nÃ£o Ã© um quadrado mÃ¡gico\n\n");
+            printf("\n\n\n\tA matriz não é um quadrado mágico\n\n");
         } else {
-            printf("A matriz Ã© um quadrado mÃ¡gico. Soma = %d\n\n", soma4);
+            printf("\n\n\n\tA matriz é um quadrado mágico. Soma = %d\n\n", soma4);
         }
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	t = getch();
 }
 
 void matrizQuadradoLatino(int matriz[i][j], int linha, int coluna) {
+	int a, b, c, x, y, n, cont = 0, soma = 0, soma2 = 0;
     if(linha != coluna) {
-        printf("O quadrado mÃ¡gico sÃ³ pode ser calculado com matriz quadrada. Volte ao menu e digite uma outra matriz.");
+        printf("\n\n\tO quadrado latino não pode ser calculado com matriz quadrada. Volte ao menu e digite uma outra matriz.");
     } else {
-
+    	n = linha;
+    	for(a = 1; a <= n; a++) {
+    		soma += a;
+		}
+		for(a = 0; a < linha; a++) {
+			for(b = 0; b < coluna; b++) {
+				soma2 += matriz[a][b];
+			}
+			if (a < linha - 1) soma2 = 0;
+		}
+		printf("%d %d", soma, soma2);
+		if(soma == soma2) {
+            printf("\n\n\n\tA matriz é um quadrado latino\n\n");
+        } else {
+            printf("\n\n\n\tA matriz não é um quadrado latino.");
+        } 
     }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
+}
+
+void matrizPermutacao(int matriz[i][j], int linha, int coluna) {
+	int c;
+	if(linha != coluna) {
+        printf("\n\n\n\tA matriz de permutação só pode ser calculada em matriz de ordem N. Volte ao menu e digite uma outra matriz.");
+    } else {
+		int verdade = 1, a, b;
+  		int soma_coluna[max], soma;
+
+		for(a = 0; a < linha; a++)
+			soma_coluna[i] = 0;
+	
+		for(a = 0; a < linha && verdade == 1; a++){
+			soma = 0;
+		    for(b = 0; b < coluna && verdade == 1; b++){
+		      if(matriz[a][b] != 1 && matriz[a][b] != 0)
+					verdade = 0;
+		      else{
+				soma_coluna[b] += matriz[a][b];
+				soma += matriz[a][b];
+		      }
+		    }
+		    if(soma != 1)
+		      verdade = 0;
+		}
+		for(a = 0; a < linha && verdade == 1; a++)
+	    	if(soma_coluna[a] != 1)
+	      		verdade = 0;
+		
+		if(verdade) printf("\n\n\n\tA matriz é de permutação");
+	    else printf("\n\n\n\tA matriz não é de permutação");
+    }
+    moveXY(5, 15, "Pressione qualquer tecla para voltar ao menu");
+	c = getch();
 }
 
 void menu() {
-    int linha, coluna, op = 1, tecla;
-    int matriz[i][j];
-    printf("\t\t\t\t MENU \t\t\t\t");
+	system("cls");
+	moveXY(32,1, "--- MENU ---");	
+	moveXY(8,3,"Ler elementos de uma matriz");
+	moveXY(8,5,"Mostrar os elementos de uma matriz");
+	moveXY(8,7,"Trocar elementos da linha X pela linha Y");
+	moveXY(8,9,"Trocar elementos da coluna X pela coluna Y");
+	moveXY(8,11,"Trocar elementos da diagonal principal com a diagonal secundária");
+	moveXY(8,13,"Verificar se a matriz é simétrica");
+	moveXY(8,15,"Verificar se uma matriz é um quadrado mágico");
+	moveXY(8,17,"Verificar se uma matriz é um quadrado latino");
+	moveXY(8,19,"Verificar se uma matriz é uma matriz de permutação");
+	moveXY(8,21,"Alterar quantidade de linhas e colunas");
+	moveXY(15,23,"Pressione ESC para sair || F1 para tela de ajuda");
+}
 
-    printf("\t\tSelecione uma opÃ§Ã£o abaixo: \t\t");
-    printf("1. Ler elementos de uma matriz.\n\n2.Mostrar os elementos de uma matriz.
-    \n\n3. Trocar elementos da linha X pela linha Y.\n\n4. Trocar elementos da coluna X pela coluna Y.
-    \n\n5. Trocar elementos da diagonal principal com a diagonal secundÃ¡ria.
-    \n\n6. Verificar se uma matriz Ã© simÃ©trica.
-    \n\n7. Verificar se uma matriz Ã© um quadrado mÃ¡gico.
-    \n\n8. Verificar se uma matriz Ã© quadrado latino.
-    \n\n9. Verificar se uma matriz Ã© matriz de permutaÃ§Ã£o.
-    \n\nPressione ESC para sair.");
-    tecla = getch();
-    if(tecla == 27) return 0;
-    while (op >= 1 && op <= 9) {
-        switch(op) {
-            case 1: lerMatriz()
-        }
-    }
-
+void menu2(int *linha, int *coluna) {
+	*linha = pegaLinha();
+    *coluna = pegaColuna();
 }
 
 int main() {
-    menu();
-    lerMatriz(matriz, linha, coluna);
-    //printf("\nMostrar Matriz\n\n");
-    //printf("\nTrocar Elementos da Linha X pela Linha Y\n");
-    //trocarElementosLinha(matriz, linha, coluna);
-    //printf("\nTrocar Elementos da Coluna X pela Coluna Y\n");
-    //trocarElementosColuna(matriz, linha, coluna);
-    //trocarDiagonal(matriz, linha, coluna);
-    //matrizSimetrica(matriz, linha, coluna);
-    //matrizQuadradoMagico(matriz, linha, coluna);
-    matrizQuadradoLatino(matriz, linha, coluna);
-    //mostrarMatriz(matriz, linha, coluna);
+	system("color 5F");
+	setlocale(LC_ALL, "");
+    int matriz[i][j], linha = 0, coluna = 0;
+    menu2(&linha, &coluna);
+    system("cls");
+	moveXY(5, 5, "->");
+	do
+	{		
+		menu();
+		moveXY(posX, posY, "->");
+		a = getch();
+		switch(a)
+		{
+			case cima:
+				if (posY > 3)
+				{
+					moveXY(5, posY, "  ");
+					posY -= 2;
+	                moveXY(5, posY, "->");				
+		     	}
+			break;
+			
+			case baixo:
+				if (posY < 21)
+				{
+					moveXY(5, posY, "  ");
+					posY += 2;
+	                moveXY(5, posY, "->");				
+		     	}
+			break;
+			
+			case enter:  // enter
+				if (posY == 3)    
+				{
+					system("cls");
+					moveXY(15, 2, "-- Ler elementos de uma matriz --");
+	               	lerMatriz(matriz, linha, coluna);
+	               	system("cls");
+		     	}
+		     	
+				if (posY == 5) 
+				{
+					system("cls");
+					moveXY(15,1, "-- Mostrar elementos de uma matriz --");
+	               	mostrarMatriz(matriz, linha, coluna);
+	               	system("cls");
+		     	}
+		     	
+		     	if (posY == 7)
+		     	{
+		     		system("cls");
+					moveXY(15,1, "-- Trocar elementos da linha X pela linha Y --");
+	               	trocarElementosLinha(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 9)
+		     	{
+		     		system("cls");
+					moveXY(15,1, "-- Trocar elementos da coluna X pela coluna Y --");
+	               	trocarElementosColuna(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 11)
+		     	{
+		     		system("cls");
+					moveXY(5,1, "-- Trocar elementos da diagonal principal com a diagonal secundária --");
+	               	trocarDiagonal(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 13)
+		     	{
+		     		system("cls");
+					moveXY(15,1, "-- Verificar se a matriz é simétrica --");
+	               	matrizSimetrica(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 15)
+		     	{
+		     		system("cls");
+					moveXY(15,1, "-- Verificar se a matriz é um quadrado mágico --");
+	               	matrizQuadradoMagico(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 17)
+		     	{
+		     		system("cls");
+					moveXY(15,1, "-- Verificar se a matriz é um quadrado latino --");
+	               	matrizQuadradoLatino(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 19)
+		     	{	
+		     		system("cls");
+					moveXY(15,1, "-- Verificar se uma matriz é uma matriz de permutação --");
+	               	matrizPermutacao(matriz, linha, coluna);
+	               	system("cls");
+				}
+				
+				if (posY == 21)
+		     	{	
+		     		system("cls");
+					menu2(&linha, &coluna);
+	               	system("cls");
+				}
+			break;
+			case esc: 
+				system("cls");
+				printf("Programa encerrado.");
+				exit(EXIT_SUCCESS);
+				break;
+			case f1:
+				system("cls");
+				ajuda();
+	    }
+    }while(1);
+	return 0;
 }
